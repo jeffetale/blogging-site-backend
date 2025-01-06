885f708b8150f7f49e8943282003a520b5fab804
jeffetale <jeff.etale@student.moringaschool.com>
2025-01-06 21:01:17 +0300

    added vercel configs

--- app/main.py
+++ app/main.py
@@ -38,7 +38,7 @@ origins = ["http://localhost:3000", "http://127.0.0.1:8000"]
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=origins,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],

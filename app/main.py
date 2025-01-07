95f1cd5025ea3135c6835d5b13fdc1274964d660
jeffetale <jeff.etale@student.moringaschool.com>
2025-01-07 23:44:55 +0300

    added other frontend url to cors

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ origins = [
     "http://localhost:3000",
     "http://127.0.0.1:8000",
     "http://localhost:8001",
-    "https://blogging-site-frontend.vercel.app"
+    "https://blogging-site-frontend.vercel.app",
     "https://blogging-site-frontend-gf461iusa-jeffetales-projects.vercel.app",
 ]
 
